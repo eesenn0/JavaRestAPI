601705c04b04df70767edb2d3baba1bc3c3444aa
eesenn0 <emreesen7@gmail.com>
2023-07-27 10:23:48 +0300

    Implementing GET methods for user resource

--- restful-web-services/src/main/java/com/eesenn0/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/eesenn0/rest/webservices/restfulwebservices/user/User.java
@@ -4,22 +4,22 @@ import java.time.LocalDate;
 
 public class User {
 	
-	private int id;
+	private Integer id;
 	private String name;
 	private LocalDate birthDate;
 	
-	public User(int id, String name, LocalDate birthDate) {
+	public User(Integer id, String name, LocalDate birthDate) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.birthDate = birthDate;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -38,7 +38,7 @@ public class User {
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
