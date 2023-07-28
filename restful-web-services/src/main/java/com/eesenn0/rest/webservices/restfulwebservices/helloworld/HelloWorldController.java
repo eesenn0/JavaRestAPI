54fa264f5de3bb10043d726d12b3d1de1394d3bc
Emre Esen <emreesen7@gmail.com>
2023-07-28 09:50:15 +0300

    Internationalization for Rest API

--- restful-web-services/src/main/java/com/eesenn0/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/eesenn0/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,22 @@
 package com.eesenn0.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+
+	public HelloWorldController(MessageSource messageSource) {
+		super();
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping("/hello-world")
 	public String helloWorld() {
@@ -20,5 +31,12 @@ public class HelloWorldController {
 	@GetMapping("/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s,", name));
+	}
+	
+	@GetMapping("/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 }
