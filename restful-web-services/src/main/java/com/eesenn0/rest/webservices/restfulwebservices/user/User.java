e8821b54b052075d31fc7e60322bbfc54d619754
Emre Esen <emreesen7@gmail.com>
2023-07-28 08:37:35 +0300

    Implementing Validations for Rest API

--- restful-web-services/src/main/java/com/eesenn0/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/eesenn0/rest/webservices/restfulwebservices/user/User.java
@@ -2,10 +2,17 @@ package com.eesenn0.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
 	
 	private Integer id;
+	
+	@Size(min = 2, message = "Name should have at least 2 characters")
 	private String name;
+	
+	@Past(message = "Birthdate should be in the past")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
